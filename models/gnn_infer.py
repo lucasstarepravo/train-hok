c761daf6b0ccab7868c335d2f4544ddb6928af1b
Lucas <l.gstarepravo@gmail.com>
2025-12-01 21:44:27 +0000

    implementing more efficient inference

--- models/gnn_infer.py
+++ models/gnn_infer.py
@@ -1,42 +1,89 @@
 import torch
-from models.labfm_moments import calc_moments_torch
+from models.labfm_moments import calc_moments_torch, monomial_power
 import numpy as np
+from torch_geometric.nn.aggr import SumAggregation
+import torch._dynamo
+from scipy.special import factorial
+from torch.nn import L1Loss
+
+if torch.cuda.is_available():
+    torch.backends.cuda.matmul.allow_tf32 = True
+    torch.backends.cudnn.allow_tf32 = True
+    torch.set_float32_matmul_precision('high') # options are highest, high and medium
+    torch._dynamo.config.capture_scalar_outputs = True # required because of attention aggregation method in gnn
+
 
 def infer(model,
-          loader):
+          loader,
+          approximation_order,
+          derivative,
+          batch_size,
+          device='cuda'):
 
+    n = int((approximation_order ** 2 + 3 * approximation_order) / 2)
+    target_moments = torch.zeros((n, 1), dtype=torch.float32)
+    if derivative == 'laplace':
+        target_moments[2] = 1.0
+        target_moments[4] = 1.0
+    elif derivative == 'x':
+        target_moments[0] = 1.0
+    elif derivative == 'y':
+        target_moments[1] = 1.0
+    else:
+        raise ValueError("derivative must be either 'laplace', 'x', or 'y'")
+
+    # Pre-computing data that will be used to compute the moments
+    target_moments = target_moments.expand(-1, batch_size).to(device=device)
+
+    mon_power = monomial_power(approximation_order)
+    inv_factorial = 1 / (factorial(mon_power[:, 0]) * factorial(mon_power[:, 1]))
+    inv_factorial = torch.tensor(inv_factorial, dtype=torch.float32, device=device)
+    mon_power = torch.tensor(mon_power, dtype=torch.float32, device=device).T
+    sum_aggr = SumAggregation().to(device=device)
+
+    model.compile()
     model.eval()
-    pred = []
+    weights = []
 
+    total_moments_err = torch.zeros((n, 1), device=device)
+    total_moments_std = torch.zeros((n, 1), device=device)
 
     with torch.no_grad():
-        for batch in loader:
-            batch = batch.to('cuda', non_blocking=True)
-            out = model(batch.x,
-                        batch.edge_index,
-                        batch.edge_attr,
-                        batch.batch)
+        with loader.enable_cpu_affinity(loader_cores=[0, 1, 2, 3]):
+            for batch_num, batch in enumerate(loader):
+                batch = batch.to('cuda', non_blocking=True)
+                out = model(batch.x,
+                            batch.edge_index,
+                            batch.edge_attr,
+                            batch.batch)
 
-            #pred_m = calc_moments_torch(batch.distances,
-            #                            out,
-            #                            batch.batch,
-            #                            approximation_order=2)
-
-            #act = calc_moments_torch(batch.distances,
-            #                            batch.y,
-            #                            batch.batch,
-            #                            approximation_order=2)
-
-
-            pred_reshape = torch.reshape(out, (int(max(batch.batch)) + 1, -1))
+                # now predict moments and add that to output
+                pred_m = calc_moments_torch(batch.distances,
+                                            out,
+                                            batch.batch,
+                                            mon_power,
+                                            inv_factorial,
+                                            sum_aggr)
 
 
-            pred.extend(pred_reshape.detach().cpu().numpy())
+                if batch.batch[-1] + 1 < batch_size:
+                    mom_diff = target_moments[:, batch.batch[-1] + 1] - pred_m
+                else:
+                    mom_diff = target_moments - pred_m
 
-    pred = np.array(pred)
+                total_moments_err += torch.sum(abs(mom_diff), dim=1)
+                total_moments_std += torch.std(mom_diff, dim=1)
 
-    return pred
+                pred_reshape = torch.reshape(out, (int(max(batch.batch)) + 1, -1))
 
+                weights.extend(pred_reshape.detach().cpu().numpy())
+
+    total_moments_err /= batch_num
+    total_moments_std /= batch_num
+
+    weights = np.array(weights)
+
+    return weights, total_moments_err, total_moments_std
 
 
 
