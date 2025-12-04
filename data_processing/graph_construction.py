43a475063126671c1f69529e31fffbbd27287dbc
Lucas <l.gstarepravo@gmail.com>
2025-12-04 09:19:46 +0000

    implementing dense and star edge connection as an option in OnDiskDataset

--- data_processing/graph_construction.py
+++ data_processing/graph_construction.py
@@ -19,6 +19,7 @@ class OnDiskStencilGraph(OnDiskDataset):
                  embedding_size: int,
                  root: str,
                  data_augmentation: bool,
+                 dense_graph: bool = False,
                  transform=None,
                  pre_transform=None,
                  pre_filter=None):
@@ -37,17 +38,21 @@ class OnDiskStencilGraph(OnDiskDataset):
         #self.transform_my_class = ToUndirected()
 
         # prebuild once
-        ls = []
-        for i in range(self.max_neighbours):
-            for j in range(self.max_neighbours):
-                if i == j: continue
-                ls.append([i,j])
+        if dense_graph:
+            ls = []
+            for i in range(self.max_neighbours):
+                for j in range(self.max_neighbours):
+                    if i == j: continue
+                    ls.append([i,j])
+            self.edges_max = torch.tensor(ls, dtype=torch.long).T
+        else:
+            self.edges_max = torch.tensor(
+                [[i, 0] for i in range(1, self.max_neighbours)],
+                dtype=torch.long).T
 
-        self.edges_max = torch.tensor(ls, dtype=torch.long).T
 
         self.schema = {
             'x': dict(dtype=torch.float32, size=(-1,2)),
-            'distances': dict(dtype=torch.float32, size=(-1,2)),
             'edge_index': dict(dtype=torch.long, size=(2,-1))
         }
 
@@ -65,13 +70,11 @@ class OnDiskStencilGraph(OnDiskDataset):
     def serialize(self, data: BaseData) -> Any:
         return {
             "x": data['x'],
-            "distances": data['distances'],
             "edge_index": data['edge_index']
         }
 
     def deserialize(self, data: Any) -> BaseData:
         return  Data(x=data['x'],
-                     distances=data['distances'],
                      edge_index=data['edge_index']
                      )
 
@@ -102,14 +105,14 @@ class OnDiskStencilGraph(OnDiskDataset):
             #rev_edge_attr = -edge_attr
             #edge_attr = torch.concat((edge_attr, rev_edge_attr))
 
+            x = torch.from_numpy(self.features[idx].copy()).to(torch.float32)
+
             # slice down to actual degree
             #num_neigh = edge_attr.shape[0]
             edge_index = self.edges_max#[:, :num_neigh].long()
             tmp = [1,0]
-            x = torch.from_numpy(self.features[idx].copy()).to(torch.float32)
-            distances = torch.from_numpy(self.features[idx, ...].copy()).to(torch.float32)
-            #rev_edge_index = edge_index[tmp, :]
-            #edge_index = torch.concat((edge_index, rev_edge_index), dim=1)
+            rev_edge_index = edge_index[tmp, :]
+            edge_index = torch.concat((edge_index, rev_edge_index), dim=1)
 
             #x = torch.ones((self.features[idx, ...].shape[0], 1), dtype=torch.float32)
             #x[0] = 1/(x.shape[0]**.5) # setting the initialisation of the node attribute to be 1/degree[i]**.5
@@ -117,7 +120,6 @@ class OnDiskStencilGraph(OnDiskDataset):
 
             data_dict = {
                         'x': x,
-                        'distances': distances,
                         'edge_index': edge_index
             }
 
